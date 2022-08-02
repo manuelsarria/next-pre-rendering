061aac38a909f58ccc0f7cae1cb802d9af4e511a
Manuel Sarrria <manuelsarria@Manuels-MacBook-Pro.local>
2022-08-02 08:19:21 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UserList from '../pages/users'
+import UserList from './pages/users'
 import App from './App'
 import './index.css'
 
